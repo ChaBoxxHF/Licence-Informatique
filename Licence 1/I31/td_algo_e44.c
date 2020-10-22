a7a5441858ec135adfd69b2fcf31f4a1fe9bfd72
Pascal 'ChaBoxxHF' CROCHARD <73274074+ChaBoxxHF@users.noreply.github.com>
2020-10-22 09:15:21 +0200

    Create td_algo_e44.c

---
+++ Licence 1/I31/td_algo_e44.c
@@ -0,0 +1,9 @@
+int main(){
+  int x;
+    do {
+        printf("Veuillez rentrer un nombre entier positif:\n");
+        scanf("%d",&x);
+    } while (x<0);
+    printf("x est positif\n");
+    printf("la racine de x est:\n %f\n",sqrt(x));
+}
