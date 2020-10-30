83a55a44030fffb5ff42b36c876c5d8ebc25377a
Pascal CROCHARD <pascal.maniak@gmail.com>
2020-10-30 09:38:56 +0100

    td algo distanciel

--- Licence_2/I31/td_algo_p3.c
+++ Licence_2/I31/td_algo_p3.c
@@ -57,7 +57,7 @@
 /*exo 101*/
 
 
-int main()
+/*int main()
 {
 	int N = 10, x = 5;
 	int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
@@ -74,7 +74,7 @@ int main()
 
 	for (int i = 0; i < N; i++)
 		printf("%d\n", A[i]);
-}
+}*/
 
 /*exo 102*/
 
