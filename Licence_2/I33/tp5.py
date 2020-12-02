de5dcc06a5c7efd9cdb5fee9e13a9d9e1678f7a2
Pascal CROCHARD <pascal.maniak@gmail.com>
2020-12-02 17:50:18 +0100

    ajout projet i32 (ainsi que tp math)

---
+++ Licence_2/I33/tp5.py
@@ -0,0 +1,16 @@
+def det(A):
+    i=0
+    while i<len(A):
+        break
+    pass
+        
+def gauss(A):
+    n=len(A)
+    m=len(A[n])
+    i=0
+    for j in range(m):
+        k=max(|A[i,j])
+
+
+#A=[[16, 12, 12],[8, 7, 6],[16, 13, 28]]
+#print(det(A))
