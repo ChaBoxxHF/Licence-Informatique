ae8519b011833a746704854128ebdbc24f3115a3
Pascal CROCHARD <pascal.maniak@gmail.com>
2020-12-07 16:19:25 +0100

    fin du projet d'i31

--- Licence_2/I31/projet_algo/finalisé/projet.c
+++ Licence_2/I31/projet_algo/finalisé/projet.c
@@ -11,26 +11,41 @@ int main ()
     gmp_randstate_t r;
     mpz_inits ( a ,b ,A ,B ,Ka ,Kb ,p ,g ,NULL);
     seed = time (NULL);
-    gmp_randinit_default (r);
+    gmp_randinit_default (r);  /*initialise un état par défaut*/
     gmp_randseed_ui (r ,seed);
     gmp_printf("Valeur de n (bit): ");
     gmp_scanf("%d", &n);
+
+
     //generation d’un nombre premier de n bits
     do
     {
-        mpz_urandomb (p ,r ,n);
-        mpz_setbit (p ,n-1);
-        mpz_nextprime (p ,p);
+        mpz_urandomb (p ,r ,n);  /*génère un nombre compris entre 0 et (2^n)-1*/
+        mpz_setbit (p ,n-1);    /*"set bit index in rop"*/
+        mpz_nextprime (p ,p);   /*Set rop to the next prime greater than op.*/
     } while (mpz_sizeinbase(p , 2) != n);
     gmp_printf("\nNombre premier p = %Zd\n", p);
+
+
     // Calcul de la racine primitive
     mpz_sqrt(g, p);
     gmp_printf("\nRacine primitive g = %Zd\n\n", g);
+
+    // Codes des personnes présentes
+    gmp_printf("Les prochains nombres demandés devront être inférieur à p !\n\n");
+    gmp_printf("Nombre d'Alice: ");
+    gmp_scanf("%Zd", &a);
+    gmp_printf("Nombre de Bob: ");
+    gmp_scanf("%Zd", &b);
+
+
     // generation de deux nombres aleatoires inferieurs à p
     mpz_urandomm ( a , r , p ) ;
     mpz_urandomm ( b , r , p ) ;
+    
+
     // Calcul des clés avec exponentiation modulaire
-    mpz_powm ( A , g , a , p ) ;
+    mpz_powm ( A , g , a , p ) ;    /*expo modulaire*/
     gmp_printf ( "Alice envoie %Zd à Bob\n\n" , A ) ;
     mpz_powm ( B , g , b , p ) ;
     gmp_printf ( "Bob envoie %Zd à Alice\n\n" , B ) ;
